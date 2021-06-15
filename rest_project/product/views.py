10e959c815f1edb3d1af895407760e59c6133f26
Zinko17 <vovazin2@gmail.com>
2021-06-15 18:21:12 +0600

    create category view

---
+++ rest_project/product/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from rest_framework import status
+from rest_framework.response import Response
+
+from .serializers import *
+from rest_framework.views import APIView
+
+
+
+class CategoryView(APIView):
+
+    def get(self,request):
+        category = Category.objects.all()
+        serializer = CategorySerializer(category,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
