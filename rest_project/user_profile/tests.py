f18d6994136b1463a0398c940533fe96684d4dde
Zinko17 <vovazin2@gmail.com>
2021-06-17 18:13:13 +0600

    create register test

--- rest_project/user_profile/tests.py
+++ rest_project/user_profile/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+class RegisterTest(APITestCase):
+
+    def setUp(self) -> None:
+        self.url = reverse('register')
+
+    def test_reg_ok(self):
+        data = {
+            "username":"111",
+            "password":"123456",
+            "check_password":"123456",
+            "profile":{
+            "full_name":"aaa",
+            "adress":"aaa",
+            "email":"aaa@gmail.com"
+            }
+            }
+        self.response = self.client.post(self.url,data,format='json')
