409d7c3711b62f7c58b7931cc16009f87db56e9c
Zinko17 <vovazin2@gmail.com>
2021-06-17 18:15:23 +0600

    create register view

--- rest_project/user_profile/views.py
+++ rest_project/user_profile/views.py
@@ -14,3 +14,13 @@ class ProfileView(APIView):
             return Response('404',status=status.HTTP_404_NOT_FOUND)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+class RegisterView(APIView):
+
+    def post(self,request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('OK',201)
+        return Response(serializer.errors,400)
